b51f488e94a041483abe392f88f4d17b3e45393c
si_choung <si_choung@tmon.co.kr>
2018-06-27 15:29:05 +0900

    first

---
+++ src/main/java/git_exercise/GitExercise.java
@@ -0,0 +1,14 @@
+package git_exercise;
+
+public class GitExercise {
+	public static void main(String[] args) {
+		GitExercise gitExercise = new GitExercise();
+		System.out.println(gitExercise.truth());
+	}
+	
+	public String truth() {
+		String truth = "정상인은 잘 생겼다.";
+		return truth;
+	}
+
+}
